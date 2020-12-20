b3ec6dc0ff8d30256feba626aba41cf2004048b2
Andrew Pikler <andrew.pikler@gmail.com>
2020-12-20 08:25:52 +0200

    support mir3

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -4,10 +4,10 @@
 
 SUBTARGET:=mt7620
 BOARDNAME:=MT7620 based boards
-FEATURES+=usb ramdisk
+FEATURES+=usb ramdisk nand
 CPU_TYPE:=24kc
 
-DEFAULT_PACKAGES += kmod-rt2800-soc wpad-basic
+DEFAULT_PACKAGES += kmod-rt2800-soc wpad-mini
 
 define Target/Description
 	Build firmware images for Ralink MT7620 based boards.
